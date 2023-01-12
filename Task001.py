ddf6c3330c749f51f248f512791fe35e9b7fb7d8
KharitonovaElena <lenuciya@mail.ru>
2023-01-13 02:19:44 +0300

    Fibonacci Series

---
+++ Task001.py
@@ -0,0 +1,13 @@
+# Задача 1. Создайте файл. Запишите в него N первых
+# элементов последовательности Фибоначчи.
+# 6 –> 1 1 2 3 5 8 
+def fibonacci(q):
+   result = [1,1]
+   for i in range(2,q):
+      fib=result[i-1]+result[i-2]
+      result.append(fib)
+   return(result)
+
+print('Задайте количество первых элементов')
+number=int(input(' последовательности Фибоначчи:  '))
+print(f'Ряд Фибоначчи {fibonacci(number)}')
