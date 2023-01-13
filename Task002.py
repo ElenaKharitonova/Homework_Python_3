8a4e3c3815ed1fec8e521c02816fc2b49eab6f8c
KharitonovaElena <lenuciya@mail.ru>
2023-01-13 04:45:30 +0300

    Fruit

--- Task002.py
+++ Task002.py
@@ -2,14 +2,18 @@
 # Выведите все фрукты, названия которых начинаются на
 # заданную букву.
 # а –> абрикос, авокадо,апельсин, айва.
+
+def printLettersFruit(let, numbers):
+    result = []
+    for index in range (len(numbers)):
+        word = str(numbers[index])
+        if word[0] == let:
+            result.append(word)
+    print(f'Фрукты на букву {let}: {result}')  
+
 data = open('Fruit.txt', encoding = 'utf-8')
 data_from_file = data.readlines()
 data.close()
 
 letter = input('Введите букву:  ')
-result = []
-for index in range (len(data_from_file)):
-    word = str(data_from_file[index])
-    if word[0] == letter:
-        result.append(word)
-print(f'Фрукты на букву {letter}: {result}')
+printLettersFruit(letter, data_from_file)
